a737604e2de65939bc2830ecf005e79e597f0564
Francisco Neto <franbezerran@gmail.com>
2022-06-19 14:02:32 -0300

    Primeiros passos da integração com API

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
-let body = document.querySelector("main");
+let input = document.querySelector("#input-crush");
 
-body.addEventListener("click", function(event){
-    console.log(event.target);
+input.addEventListener("keydown", function(event){
+    if (event.key == "Enter"){
+        const yourName = input.value;
+        return requestToAPI(yourName);
+    }
 });
+
+function requestToAPI(name) {
+    const strToAI = strTreatment(name);
+    // const request = new XMLHttpRequest();
+
+    // request.open("");
+
+    spaceTreatment();
+    alert(strToAI);
+}
+
+function strTreatment(subStr) {
+    return `pessoa com o nome ${subStr} desde que não seja uma criança`;
+}
+
+function spaceTreatment() {
+    let space = document.querySelector(".final-img");
+
+    space.classList.add("desapear");
+}

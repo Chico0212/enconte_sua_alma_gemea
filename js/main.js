9fcdaefdc9fabc18d7b13df326fc582d63b3f38f
Francisco Neto <bezerra.neto@academico.ifpb.edu.br>
2022-06-20 22:28:18 -0300

    Finalizado

--- js/main.js
+++ js/main.js
@@ -4,10 +4,22 @@ inputSpace.addEventListener("keydown", function(event){
     if (event.key == "Enter"){
         let button = document.getElementById("the-button");
         const text = this.value;
-        button.classList.remove("desapear");
+
+        if (!inputValidation(text))
+            return;
+
+        buttonTreatment(button);
+
         button.onclick = function() {
             alert(`${text}, amor da sua vida vai aparecer, relaxe`);
+            alert("Mas antes que ele apareça, veja isso!!");
         };
     }
 });
 
+
+function buttonTreatment(button) {
+    button.classList.remove("desapear");
+    let ancor = button.parentNode;
+    ancor.href = "https://www.youtube.com/shorts/YJWSAJjkTsI";
+}
---
+++ js/validation.js
@@ -0,0 +1,8 @@
+function inputValidation(str){
+    if (str == ""){
+        alert("Eu não consigo achar o amor da sua vida, se eu não souber seu nome");
+        return false;
+    }
+
+    return true;
+}
